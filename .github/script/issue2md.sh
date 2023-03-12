a353f5d2fc4722576b962054c76e6fad5be4247f
tTdly-Old <92563872+tTdly-Old@users.noreply.github.com>
2023-03-12 16:47:21 +0800

    用api处理文章

--- .github/script/issue2md.sh
+++ .github/script/issue2md.sh
@@ -1,8 +1,17 @@
-# $1 title $2 body $3 date $4 number
-# $1 title $2 body $3 date $4 number
-HEADYML="---\ntitle: $1\ndate: $3\n---"
+RESULT=$(curl -L \
+  -H "Accept: application/vnd.github+json" \
+  -H "Authorization: Bearer $4"\
+  -H "X-GitHub-Api-Version: 2022-11-28" \
+  https://api.github.com/repos/$1/$2/issues/$3
+)
+
+TITLE=$(echo $RESULT | jq .title)
+NUMBER=$(echo $RESULT | jq .number)
+BODY=$(echo $RESULT | jq .body)
+TIME=$(echo $RESULT | jq .created_at)
+HEADYML="---\ntitle: $TITLE\ndate: $TIME\n---"
 if [ ! -d "pages" ];then
     mkdir pages
 fi
-echo -e $HEADYML > pages/$4.md
-echo -e $2 >> pages/$4.md
+echo -e $HEADYML > pages/$NUMBER.md
+echo -e $BODY >> pages/$NUMBER.md
