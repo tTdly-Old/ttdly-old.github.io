3ec4d47aed982d24d5a07fbac13c8ca9c4796cc8
tutoudenglongyu <tutoudenglongyu@163.com>
2023-03-13 10:07:52 +0800

    删除无用工作流程修改部分变量

--- .github/script/issue2md.sh
+++ .github/script/issue2md.sh
@@ -1,8 +1,8 @@
 RESULT=$(curl -L \
   -H "Accept: application/vnd.github+json" \
-  -H "Authorization: Bearer $4"\
+  -H "Authorization: Bearer $3"\
   -H "X-GitHub-Api-Version: 2022-11-28" \
-  https://api.github.com/repos/$1/$2/issues/$3
+  https://api.github.com/repos/$1/issues/$2
 )
 
 TITLE=$(echo $RESULT | jq .title)
